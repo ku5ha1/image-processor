328fc3fae341a179a08b75e12c96572f36e91127
kushalraga <kushalraga20@gmail.com>
2025-09-22 15:25:14 +0530

    file type validation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File   
+from fastapi import FastAPI, UploadFile, File, HTTPException
 
 app = FastAPI() 
 
@@ -6,6 +6,19 @@ app = FastAPI()
 async def read_root():
     return {"message": "API running successfully"}
 
+ALLOWED_IMAGE_TYPES = {
+    "image/jpg",
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+    "image/gif",
+}
+
 @app.post("/upload-image")
 async def upload_image(file: UploadFile = File(...)):
+    if file.content_type not in ALLOWED_IMAGE_TYPES:
+        raise HTTPException(
+            status_code=400,
+            detail="Attached file is not an image."
+        )
     return {"filename": file.filename}
