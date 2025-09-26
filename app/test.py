971047d4584840186f911318c53ae86cff579ee5
kushalraga <kushalraga20@gmail.com>
2025-09-26 22:12:35 +0530

    basic pillow operations

---
+++ app/test.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+im = Image.open('app/test.png')
+
+# print(im.format)
+# print(im.size)
+# print(im.mode)
+# print(im.filename)
+# print(im.getcolors) 
+
+# print(im.show())
+
+# im.save('test.jpeg')  
+
+# output = im.resize((250, 250))
+# output.save('app/output.jpg')
